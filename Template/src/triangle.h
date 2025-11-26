01a57d21696e20b34321fac09ed19f4bd7f988b4
Joost Kootstra <joost.kootstra@gmail.com>
2025-11-26 17:10:51 +0100

    triangle start

---
+++ Template/src/triangle.h
@@ -0,0 +1,49 @@
+//
+// Created by joost on 26/11/2025.
+//
+
+#ifndef ADVANCED_GRAPHICS_TRIANGLE_H
+
+#define ADVANCED_GRAPHICS_TRIANGLE_H
+
+#include <cmath>
+#include <memory>
+
+#include "primitive.h"
+#include "Vec3.h"
+
+class Triangle : public Primitive
+{
+    public:
+        Triangle(const Point3& Q, const vec3& u, const vec3& v, shared_ptr<Material> mat)
+        : Q(Q), u(u), v(v), mat(mat)
+        {
+            auto n = cross(u, v);
+            normal = unit_vector(n);
+            D = dot(normal, Q);
+            w = n / dot(n, n);
+        }
+
+        bool hit(const Ray& r, Interval ray_t, Hit_record& rec) const override
+        {
+            auto denom = dot(normal, r.direction());
+
+            if (std::fabs(denom) < 1e-8) return false;
+
+            auto t = (D-dot(normal, r.origin()))/ denom;
+            if (!ray_t.contains(t)) return false;
+
+        }
+
+    private:
+        Point3 Q;
+        vec3 u,v;
+        vec3 w;
+        shared_ptr<Material> mat;
+        vec3 normal;
+        double D;
+
+};
+
+
+#endif //ADVANCED_GRAPHICS_TRIANGLE_H
